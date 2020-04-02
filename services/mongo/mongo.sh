df9fbf7e07361166cb8be5eec5e75bf673810add
Phu Thai HO <phu.ho@nokia.com>
2020-04-02 20:31:19 +0200

    Add mongodb

--- services/mongo/mongo.sh
+++ services/mongo/mongo.sh
@@ -3,7 +3,8 @@ var user = '$MONGO_INITDB_ROOT_USERNAME';
 var passwd = '$MONGO_INITDB_ROOT_PASSWORD';
 var admin = db.getSiblingDB('admin');
 admin.auth(user, passwd);
-var xpmdb = db.getSiblingDB('testdb');
+var xpmdb = db.getSiblingDB('test');
 xpmdb.createCollection('users');
-xpmdb.createUser({user:'tester', pwd:'tester!2020', roles: [{role:'readWrite', db: 'testdb'}]});
+xpmdb.createCollection('talks');
+xpmdb.createUser({user:'test', pwd:'test!2020', roles: [{role:'readWrite', db: 'test'}]});
 EOF
